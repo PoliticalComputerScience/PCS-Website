7a5588cc25ec2d621af17579e6d8fd0f47616416
Noah <na933950@gmail.com>
2024-08-10 14:01:59 -0700

    Updated Navbar to route correctly

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import logo from "/images/logo.jpg";
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 import {
   getDefaultEmphasisTextStyles,
   defaultFlexRowStyles,
@@ -15,7 +15,7 @@ import {
   Menu,
 } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const theme = useTheme();
@@ -24,29 +24,49 @@ const Navbar = () => {
   interface LinkProps {
     name: string;
     buttonVariant: string;
-    link: string;
+    link: string | LinkProps[];
     color?: string;
   }
 
   const navbarContainerStyles: SxProps = {
     ...defaultFlexRowStyles,
     flexDirection: {
-        md: 'row',
-        sx: 'column'
+      md: "row",
+      sx: "column",
     },
     px: 1.5,
     py: 1.2,
     justifyContent: "space-between",
     height: "4.5rem",
-    width: '100%',
-    position: 'fixed',
-    backgroundColor: 'rgba(256, 256, 256, 0.7)',
-    backdropFilter: 'blur(11px)'
+    width: "100%",
+    position: "fixed",
+    backgroundColor: "rgba(256, 256, 256, 0.7)",
+    backdropFilter: "blur(11px)",
   };
 
   const imageStyles: SxProps = {
     height: "100%",
     objectFit: "contain",
+  };
+
+  // Styles for a button in the dropdown
+  const dropdownButtonStyles: SxProps = {
+    width: "100%",
+    height: "100%",
+    padding: "0.5rem",
+  };
+
+  // Styles for the button itself
+  const buttonStyles: SxProps = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "0.5rem",
+  };
+
+  const menuItemStyles: SxProps = {
+    margin: 0,
+    padding: 0,
   };
 
   const pages: LinkProps[] = [
@@ -63,7 +83,28 @@ const Navbar = () => {
     {
       name: "what we do",
       buttonVariant: "text",
-      link: "/what-we-do",
+      link: [
+        {
+          name: "projects",
+          buttonVariant: "text",
+          link: "/what-we-do/projects",
+        },
+        {
+          name: "editorials",
+          buttonVariant: "text",
+          link: "/what-we-do/editorials",
+        },
+        {
+          name: "research",
+          buttonVariant: "text",
+          link: "/what-we-do/research",
+        },
+        {
+          name: "external events",
+          buttonVariant: "text",
+          link: "/what-we-do/external-events",
+        },
+      ],
     },
     {
       name: "join us",
@@ -74,12 +115,72 @@ const Navbar = () => {
   ];
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [whatWeDoAnchorEl, setWhatWeDoAnchorEl] = useState<null | HTMLElement>(
+    null
+  );
   const open = Boolean(anchorEl);
+  const whatWeDoOpen = Boolean(whatWeDoAnchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
+  const handleClickWhatWeDo = (event: React.MouseEvent<HTMLElement>) => {
+    setWhatWeDoAnchorEl(event.currentTarget);
+  };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+  const handleCloseWhatWeDo = () => {
+    setWhatWeDoAnchorEl(null);
+  };
+
+  const customDropdown = (name: string, link: LinkProps[]): ReactElement => {
+    return (
+      <Box>
+        <Button
+          id="demo-positioned-button"
+          aria-controls={open ? "demo-positioned-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClickWhatWeDo}
+          sx={buttonStyles}
+        >
+          {name}
+          <FontAwesomeIcon icon={faCaretDown} />
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={whatWeDoAnchorEl}
+          open={whatWeDoOpen}
+          onClose={handleCloseWhatWeDo}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          {link.map((page) => (
+            <MenuItem sx={menuItemStyles}>
+              {
+                // @ts-expect-error there is a known bug with MUI types where a string can be used by TypeScript is not
+                // able to parse the autmoatic type conversion
+                <Button
+                  href={page.link}
+                  variant={page.buttonVariant}
+                  color={page.color ? page.color : "inherit"}
+                  sx={dropdownButtonStyles}
+                >
+                  {page.name}
+                </Button>
+              }
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    );
   };
 
   const mobileNavLinks = (
@@ -108,8 +209,8 @@ const Navbar = () => {
           horizontal: "left",
         }}
       >
-        {pages.map(({ name, buttonVariant, link, color }) =>
-          color ? (
+        {pages.map(({ name, buttonVariant, link, color = "inherit" }) =>
+          typeof link === "string" ? (
             <MenuItem>
               {
                 // @ts-expect-error there is a known bug with MUI types where a string can be used by TypeScript is not
@@ -120,15 +221,7 @@ const Navbar = () => {
               }
             </MenuItem>
           ) : (
-            <MenuItem>
-              {
-                // @ts-expect-error there is a known bug with MUI types where a string can be used by TypeScript is not
-                // able to parse the autmoatic type conversion
-                <Button href={link} variant={buttonVariant}>
-                  {name}
-                </Button>
-              }
-            </MenuItem>
+            customDropdown(name, link)
           )
         )}
       </Menu>
@@ -138,18 +231,14 @@ const Navbar = () => {
   const desktopNavLinks = (
     <Box sx={{ ...defaultFlexRowStyles, display: { md: "flex", xs: "none" } }}>
       {pages.map(({ name, buttonVariant, link, color }) =>
-        color ? (
+        typeof link === "string" ? (
           // @ts-expect-error there is a known bug with MUI types where a string can be used by TypeScript is not
           // able to parse the autmoatic type conversion
           <Button href={link} variant={buttonVariant} color={color}>
             {name}
           </Button>
         ) : (
-          // @ts-expect-error there is a known bug with MUI types where a string can be used by TypeScript is not
-          // able to parse the autmoatic type conversion
-          <Button href={link} variant={buttonVariant}>
-            {name}
-          </Button>
+          customDropdown(name, link)
         )
       )}
     </Box>
@@ -157,15 +246,15 @@ const Navbar = () => {
 
   const leftNavbar = (
     <Box sx={defaultFlexRowStyles}>
-        <Box component="img" src={logo} sx={imageStyles} />
-        <Typography variant="h2" component="div">
-          <Box component="span" sx={defaultEmphasisTextStyles}>
-            PCS{" "}
-          </Box>
-          @ Berkeley
-        </Typography>
-      </Box>
-  )
+      <Box component="img" src={logo} sx={imageStyles} />
+      <Typography variant="h2" component="div">
+        <Box component="span" sx={defaultEmphasisTextStyles}>
+          PCS{" "}
+        </Box>
+        @ Berkeley
+      </Typography>
+    </Box>
+  );
 
   return (
     <Box sx={navbarContainerStyles}>
